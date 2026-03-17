8cbe26305c9a5ab229656286851039a2cf0cae23
Ngoc Huyen <huyendtnk24411e@st.uel.edu.vn>
2026-03-17 23:10:17 +0700

    Delete test directory

--- test/test _product_mainwindow.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from ui.ProductMainWindowEx import ProductMainWindowEx
-
-app=QApplication([])
-gui=ProductMainWindowEx()
-gui.setupUi(QMainWindow())
-gui.showWindow()
-app.exec()
