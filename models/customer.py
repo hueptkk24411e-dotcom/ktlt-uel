3a9fc8717fa9f44a670d798cb39a77fab0d667da
Ngoc Huyen <huyendtnk24411e@st.uel.edu.vn>
2026-03-17 23:09:43 +0700

    Delete models directory

--- models/customer.py
+++
@@ -1,9 +0,0 @@
-class Customer:
-    def __init__(self,CustomerUserName=None,PhoneNumber=None,Type=None,Rating=None):
-        self.CustomerUserName=CustomerUserName
-        self.PhoneNumber=PhoneNumber
-        self.Type=Type
-        self.Rating=Rating
-    def __str__(self):
-        infor=f"{self.CustomerUserName}\t{self.PhoneNumber}\t{self.Type}\t{self.Rating}"
-        return infor
